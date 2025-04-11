3f1a28ce550f6360a2b7abe98d1c95dd46e8b138
whitecity01 <kh081104@gmail.com>
2025-04-11 23:26:08 +0900

    feat: 라우팅 추가

--- src/routes/lobby.js
+++ src/routes/lobby.js
@@ -1,12 +1,15 @@
-import LobbyPage from "pages/LobbyPage";
+import LobbyLayout, { loader as lobbyLoader } from "pages/lobby/LobbyLayout";
+import LobbyPage from "pages/lobby/LobbyPage";
 
 const lobby = [
   {
     path: "lobby",
+    element: <LobbyLayout />,
     children: [
       {
         index: true,
         element: <LobbyPage />,
+        loader: lobbyLoader,
       },
     ],
   },

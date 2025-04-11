c4cfa3684631fe9162f6c7c8a45d48636d4cf89f
whitecity01 <kh081104@gmail.com>
2025-04-11 23:21:40 +0900

    feat: url 파일 추가

---
+++ src/constants/url.js
@@ -0,0 +1,7 @@
+export const ROOT_URL = "/"; // 루트
+
+export const SIGN_IN_URL = "/signin"; // 로그인
+export const SIGN_UP_URL = "/signup"; // 회원가입
+export const ACCOUNT_URL = "/find/account"; // 비밀번호 찾기
+
+export const LOBBY_URL = "/lobby"; // 로비

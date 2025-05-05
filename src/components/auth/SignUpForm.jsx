96d50ecea8ddc4860b9f5fe693ea11184e290f4a
Jiwon Park <parkjiwon0821@naver.com>
2025-05-06 00:11:27 +0900

    feat: 입력 정보 확인 시 disabled처리

--- src/components/auth/SignUpForm.jsx
+++ src/components/auth/SignUpForm.jsx
@@ -1,4 +1,5 @@
 import useSignUpForm from "hooks/auth/useSignUpForm";
+import { useState } from "react";
 import { Form } from "react-router-dom";
 import { sendEmailCodeAPI, verifyEmailCodeAPI } from "services/auth/auth";
 import { checkNicknameDuplicateAPI } from "services/user/user";
@@ -34,12 +35,17 @@ const SignUpForm = ({ errorMessage }) => {
 		emailError,
 	} = useSignUpForm();
 
+	const [isNicknameConfirmed, setIsNicknameConfirmed] = useState(false);
+	const [isEmailCodeRequested, setIsEmailCodeRequested] = useState(false); // 인증요청 클릭 여부
+	const [isEmailVerified, setIsEmailVerified] = useState(false); // 인증 성공 여부
+
 	const checkNicknameDuplicate = async () => {
 		if (!nickname) return;
 
 		try {
 			const res = await checkNicknameDuplicateAPI(nickname);
 			alert(res.message);
+			setIsNicknameConfirmed(true);
 		} catch (e) {
 			handleApiError(e, {
 				409: () => alert("이미 사용 중인 닉네임입니다."),
@@ -52,6 +58,7 @@ const SignUpForm = ({ errorMessage }) => {
 		try {
 			const res = await sendEmailCodeAPI(email);
 			alert(res.message);
+			setIsEmailCodeRequested(true);
 		} catch (e) {
 			handleApiError(e, {
 				404: () => alert("이미 가입된 이메일입니다."),
@@ -65,6 +72,7 @@ const SignUpForm = ({ errorMessage }) => {
 		try {
 			const res = await verifyEmailCodeAPI(email, emailCode);
 			alert(res.message);
+			setIsEmailVerified(true);
 		} catch (e) {
 			handleApiError(e, {
 				400: () => alert("인증번호가 일치하지 않습니다."),
@@ -90,11 +98,13 @@ const SignUpForm = ({ errorMessage }) => {
 						value={nickname}
 						onChange={(e) => handleNicknameChange(e.target.value)}
 						name="nickname"
+						disabled={isNicknameConfirmed}
 					/>
 					<button
 						type="button"
 						className="check-nickname-btn"
 						onClick={checkNicknameDuplicate}
+						disabled={isNicknameConfirmed}
 					>
 						닉네임 중복 확인
 					</button>
@@ -137,11 +147,13 @@ const SignUpForm = ({ errorMessage }) => {
 						value={email}
 						onChange={(e) => handleEmailChange(e.target.value)}
 						name="email"
+						disabled={isEmailVerified}
 					/>
 					<button
 						type="button"
 						className="request-code-btn"
 						onClick={requestEmailCode}
+						disabled={isEmailVerified}
 					>
 						인증번호 요청
 					</button>
@@ -155,11 +167,13 @@ const SignUpForm = ({ errorMessage }) => {
 						value={emailCode}
 						onChange={(e) => handleEmailCodeChange(e.target.value)}
 						name="emailCode"
+						disabled={!isEmailCodeRequested || isEmailVerified}
 					/>
 					<button
 						type="button"
 						className="verify-code-btn"
 						onClick={confirmEmailCode}
+						disabled={!isEmailCodeRequested || isEmailVerified}
 					>
 						인증번호 확인
 					</button>

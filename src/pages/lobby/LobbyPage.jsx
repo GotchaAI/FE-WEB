370a164a44a93da98b8b74e8ab772b85fcc4be1b
whitecity01 <kh081104@gmail.com>
2025-05-07 12:48:45 +0900

    feat: LobbyLayout -> LobbyPage로 통합

--- src/pages/lobby/LobbyPage.jsx
+++ src/pages/lobby/LobbyPage.jsx
@@ -1,4 +1,8 @@
-import useGameSocket from "hooks/useGameSocket";
+import Lobby from "components/lobby/Lobby";
+import { ROOT_URL } from "constants/url";
+import { tokenReissueAPI } from "services/auth/auth";
+import "styles/pages/lobby/LobbyPage.scss";
+import { getAuthToken } from "utils/token";
 
 const LobbyPage = () => {
   // 소켓 연결 테스트(지워도 됨!)
@@ -7,7 +11,30 @@ const LobbyPage = () => {
 
   useGameSocket({ nickName, roomId });
 
-  return <div className="robby-page-container">lobby</div>;
+  return (
+    <div className="lobby-page-container">
+      <Lobby />
+    </div>
+  );
 };
 
 export default LobbyPage;
+
+export const loader = async () => {
+  const { accessToken, setAccessToken } = getAuthToken();
+
+  if (!accessToken) {
+    // 토큰 재발급
+    try {
+      const res = await tokenReissueAPI();
+      const newAccessToken = res.accessToken;
+      const newExpireTime = res.expiredAt;
+
+      setAccessToken(newAccessToken, newExpireTime);
+    } catch (e) {
+      return redirect(ROOT_URL);
+    }
+  }
+
+  return;
+};
--- src/routes/lobby.js
+++ src/routes/lobby.js
@@ -1,17 +1,9 @@
-import LobbyLayout, { loader as lobbyLoader } from "pages/lobby/LobbyLayout";
 import LobbyPage from "pages/lobby/LobbyPage";
 
 const lobby = [
   {
     path: "lobby",
-    element: <LobbyLayout />,
-    children: [
-      {
-        index: true,
-        element: <LobbyPage />,
-        loader: lobbyLoader,
-      },
-    ],
+    element: <LobbyPage />,
   },
 ];
 

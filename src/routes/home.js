0b2da05fcec7520797e662bb37c0af007f829fd5
whitecity01 <kh081104@gmail.com>
2025-05-07 15:06:02 +0900

    feat: HomeLayout -> HomePage 통합

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,18 +1,10 @@
-import HomeLayout, { loader as homeLoader } from "pages/home/HomeLayout";
-import HomePage from "pages/home/HomePage";
+import HomePage, { loader as homeLoader } from "pages/home/HomePage";
 
 const home = [
   {
     path: "/",
-
-    element: <HomeLayout />,
+    element: <HomePage />,
     loader: homeLoader,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-    ],
   },
 ];
 

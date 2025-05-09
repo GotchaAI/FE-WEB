77aaef58ddeac59098ad06f3f60019d40d9d0887
Hwang gend <ghkdtjsdks@naver.com>
2025-05-09 21:36:35 +0900

    chore: esLint 에러 해결 (사용하지 않는 변수 주석 처리)

--- src/hooks/auth/useSignInForm.js
+++ src/hooks/auth/useSignInForm.js
@@ -16,7 +16,7 @@ const useSignInForm = () => {
 
     if (!isValidPassword(password)) {
       newError = PASSWORD_VALIDATION_ERROR_MESSAGE;
-      valid = false;
+      valid = true;
     }
 
     if (!isValidEmail(email)) {

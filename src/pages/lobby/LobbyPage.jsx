04de7f950a73709d4394322c771a8cbc2504f9f8
Hwang gend <ghkdtjsdks@naver.com>
2025-05-09 20:53:42 +0900

    feat: 에러메시지 props 추가 (추후 추가 예정)

--- src/pages/lobby/LobbyPage.jsx
+++ src/pages/lobby/LobbyPage.jsx
@@ -1,15 +1,16 @@
 import LobbyChatting from "components/lobby/LobbyChatting";
 import useGameSocket from "hooks/useGameSocket";
+import { useActionData } from "react-router-dom";
 
 const LobbyPage = () => {
   // 소켓 연결 테스트(지워도 됨!)
   const nickName = "jiwon";
   const roomId = "1234";
-
+  const errorMessage = useActionData();
   //useGameSocket({ nickName, roomId });
   return (
     <div className="robby-page-container">
-      <LobbyChatting />
+      <LobbyChatting errorMessage={errorMessage} />
     </div>
   );
 };

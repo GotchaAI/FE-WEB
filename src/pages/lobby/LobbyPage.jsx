0de98beba0aad47305aadb434b26ab90c511ed2d
Hwang gend <ghkdtjsdks@naver.com>
2025-05-09 21:40:04 +0900

    fix: esLint 에러 수정 (사용하지 않는 변수 주석처리)

--- src/pages/lobby/LobbyPage.jsx
+++ src/pages/lobby/LobbyPage.jsx
@@ -1,11 +1,11 @@
 import LobbyChatting from "components/lobby/LobbyChatting";
-import useGameSocket from "hooks/useGameSocket";
+//import useGameSocket from "hooks/useGameSocket";
 import { useActionData } from "react-router-dom";
 
 const LobbyPage = () => {
   // 소켓 연결 테스트(지워도 됨!)
-  const nickName = "jiwon";
-  const roomId = "1234";
+  //const nickName = "jiwon";
+  //const roomId = "1234";
   const errorMessage = useActionData();
   //useGameSocket({ nickName, roomId });
   return (

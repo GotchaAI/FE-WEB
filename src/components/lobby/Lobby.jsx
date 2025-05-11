2e0ade83dad74f3cc177112931d9f27c30f48643
whitecity01 <kh081104@gmail.com>
2025-05-11 17:20:10 +0900

    feat: 로비 컴포넌트 UI 구현

--- src/components/lobby/Lobby.jsx
+++ src/components/lobby/Lobby.jsx
@@ -1,5 +1,56 @@
+import logoImage from "assets/commons/logo.png";
+import springImg from "assets/commons/spring.png";
+import Mypage from "commons/options/Mypage";
+import Settings from "commons/options/Settings";
+import Sound from "commons/options/Sound";
+import Friend from "components/lobby/friend/Friend";
+import WaitingRoom from "components/lobby/waiting/WaitingRoom";
+import Profile from "pages/home/Profile";
+import { useState } from "react";
+import "styles/components/lobby/Lobby.scss";
+
 const Lobby = () => {
-  return <div>로비요</div>;
+  const [navType, setNavType] = useState("type-a");
+
+  return (
+    <div className="lobby-container">
+      <header className="header-container">
+        <Profile />
+        <img className="logo-img" src={logoImage} alt="로고" />
+        <div className="options-container">
+          <Settings />
+          <Sound />
+          <Mypage />
+        </div>
+      </header>
+      <div className="body-container">
+        <Friend />
+        <div className="main-content-container">
+          <img src={springImg} alt="스프링" />
+          <div className="main-content-nav-container">
+            <button
+              className={`a-btn ${navType === "type-a" ? "active" : ""}`}
+              onClick={() => setNavType("type-a")}
+            >
+              A
+            </button>
+            <button
+              className={`b-btn ${navType === "type-b" ? "active" : ""}`}
+              onClick={() => setNavType("type-b")}
+            >
+              B
+            </button>
+          </div>
+          <div className="main-content-layout">
+            {/* 상태나 라우팅에 따라 WaitingRoom or Mypage로 */}
+            <WaitingRoom />
+          </div>
+        </div>
+
+        <div className="chat-container"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Lobby;

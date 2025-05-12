2ca2a3651c5e3e8a995c086c523025ae2f79a40f
Jiwon Park <parkjiwon0821@naver.com>
2025-05-12 15:25:04 +0900

    feat: 커스텀 체크박스 추가

--- src/pages/lobby/LobbyPage.jsx
+++ src/pages/lobby/LobbyPage.jsx
@@ -1,13 +1,18 @@
+import CheckBox from "commons/svgs/CheckBox";
 import useGameSocket from "hooks/useGameSocket";
 
 const LobbyPage = () => {
-  // 소켓 연결 테스트(지워도 됨!)
-  const nickName = "jiwon";
-  const roomId = "1234";
+	// 소켓 연결 테스트(지워도 됨!)
+	const nickName = "jiwon";
+	const roomId = "1234";
 
-  useGameSocket({ nickName, roomId });
+	useGameSocket({ nickName, roomId });
 
-  return <div className="robby-page-container">lobby</div>;
+	return (
+		<div className="robby-page-container">
+			<CheckBox label="고수" />
+		</div>
+	);
 };
 
 export default LobbyPage;

0bda7339793b17d4719416b566e90508802bc066
Jiwon Park <parkjiwon0821@naver.com>
2025-05-12 17:55:13 +0900

    design: 게임 준비 버튼 추가

--- src/components/lobby/waiting/WaitingRoom.jsx
+++ src/components/lobby/waiting/WaitingRoom.jsx
@@ -3,6 +3,7 @@ import "styles/components/lobby/waiting/WaitingRoom.scss";
 import PlayerSlot from "./PlayerSlot";
 import CheckBox from "commons/svgs/CheckBox";
 import GameStartButton from "commons/svgs/GameStartButton";
+import GameReadyButton from "commons/svgs/GameReadyButton";
 
 const WaitingRoom = () => {
 	const players = [
@@ -54,6 +55,7 @@ const WaitingRoom = () => {
 
 				<div className="start-btn-wrapper">
 					<GameStartButton />
+					{/* <GameReadyButton /> */}
 				</div>
 			</div>
 		</div>

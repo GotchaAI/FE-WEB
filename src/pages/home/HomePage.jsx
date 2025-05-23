cc7df31ce8150491921e1c4ae7e4912b52a2e2f8
Hwang gend <ghkdtjsdks@naver.com>
2025-05-23 22:20:59 +0900

    feat: 홈페이지 공지사항 및 고객센터 미리보기 구현

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -4,78 +4,127 @@ import rabbitBot from "assets/rabbit-bot.png";
 import rabbit from "assets/rabbit-hand-up.png";
 import StartButton from "commons/svgs/StartButton";
 import {
-	ANNOUNCE_URL,
-	CHARACTER_INTRO_URL,
-	LOBBY_URL,
-	RANKING_URL,
-	SERVICE_CENTER_URL,
-	SIGN_IN_URL,
+  ANNOUNCE_URL,
+  CHARACTER_INTRO_URL,
+  LOBBY_URL,
+  RANKING_URL,
+  SERVICE_CENTER_URL,
+  SIGN_IN_URL,
 } from "constants/url";
 import Profile from "pages/home/Profile";
-import { Link, useLoaderData, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  Link,
+  useLoaderData,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
 import { tokenReissueAPI } from "services/auth/auth";
 import "styles/pages/home/HomePage.scss";
 import { getAuthToken } from "utils/token";
 
 const HomePage = () => {
-	const { isSignIn } = useLoaderData();
+  const { isSignIn } = useLoaderData();
+  const navigate = useNavigate();
 
-	const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const navType = searchParams.get("type") || "notice";
 
-	const handleStartBtn = () => {
-		navigate(LOBBY_URL);
-	};
+  const handleStartBtn = () => {
+    navigate(LOBBY_URL);
+  };
+  const [data, setData] = useState([]);
 
-	return (
-		<div className="home-page-container">
-			<header className="home-page-header">
-				<img src={logo} alt="logo" className="home-logo-img" />
-				<nav className="home-page-nav-container">
-					<Link to={ANNOUNCE_URL}>공지사항</Link>
-					<Link to={RANKING_URL}>랭킹</Link>
-					<Link to={CHARACTER_INTRO_URL}>캐릭터 소개</Link>
-					<Link to={SERVICE_CENTER_URL}>고객센터</Link>
-				</nav>
-				{isSignIn ? (
-					<Profile />
-				) : (
-					<Link to={SIGN_IN_URL} className="sign-in-btn">
-						로그인
-					</Link>
-				)}
-			</header>
+  useEffect(() => {
+    if (navType === "notice") {
+      // 공지사항 더미 데이터
+      setData(["공지 1", "공지 2", "공지 3"]);
+    } else if (navType === "help") {
+      // 고객센터 더미 데이터
+      setData(["문의 1", "문의 2", "문의 3"]);
+    }
+  }, [navType]);
 
-			<div className="background-container">
-				<img src={rabbit} alt="공원 위 토끼" className="left-background-img" />
-				<img
-					src={rabbitBot}
-					alt="공원 위 토끼 봇"
-					className="right-background-img"
-				/>
-				<img src={intro_msg} alt="게임 소개 글" className="intro-msg-img" />
-			</div>
+  return (
+    <div className="home-page-container">
+      <div className="home-fixed-section">
+        <header className="home-page-header">
+          <img src={logo} alt="logo" className="home-logo-img" />
+          <nav className="home-page-nav-container">
+            <Link to={ANNOUNCE_URL}>공지사항</Link>
+            <Link to={RANKING_URL}>랭킹</Link>
+            <Link to={CHARACTER_INTRO_URL}>캐릭터 소개</Link>
+            <Link to={SERVICE_CENTER_URL}>고객센터</Link>
+          </nav>
+          {isSignIn ? (
+            <Profile />
+          ) : (
+            <Link to={SIGN_IN_URL} className="sign-in-btn">
+              로그인
+            </Link>
+          )}
+        </header>
 
-			<StartButton onClick={handleStartBtn} />
-		</div>
-	);
+        <div className="background-container">
+          <img
+            src={rabbit}
+            alt="공원 위 토끼"
+            className="left-background-img"
+          />
+          <img
+            src={rabbitBot}
+            alt="공원 위 토끼 봇"
+            className="right-background-img"
+          />
+          <img src={intro_msg} alt="게임 소개 글" className="intro-msg-img" />
+        </div>
+
+        <StartButton onClick={handleStartBtn} />
+      </div>
+
+      <div className="home-scroll-section">
+        <div className="content-wrapper">
+          <div className="information-type-container">
+            <Link
+              to="?type=notice"
+              className={`a-btn ${navType === "notice" ? "active" : ""}`}
+            >
+              공지사항
+            </Link>
+            <Link
+              to="?type=help"
+              className={`b-btn ${navType === "help" ? "active" : ""}`}
+            >
+              고객센터
+            </Link>
+          </div>
+          <div className="information-content-layout">
+            {data.map((item, idx) => (
+              <p key={idx}>{item}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
 
 export const loader = async () => {
-	const { accessToken, setAccessToken } = getAuthToken();
+  const { accessToken, setAccessToken } = getAuthToken();
 
-	if (!accessToken) {
-		// 토큰 재발급
-		try {
-			const res = await tokenReissueAPI();
-			const newAccessToken = res.accessToken;
-			const expireTime = res.expiredAt;
-			setAccessToken(newAccessToken, expireTime);
-		} catch (e) {
-			console.error(e);
-			return { isSignIn: false };
-		}
-	}
-	return { isSignIn: true };
+  if (!accessToken) {
+    // 토큰 재발급
+    try {
+      const res = await tokenReissueAPI();
+      const newAccessToken = res.accessToken;
+      const expireTime = res.expiredAt;
+      setAccessToken(newAccessToken, expireTime);
+    } catch (e) {
+      console.error(e);
+      return { isSignIn: false };
+    }
+  }
+  return { isSignIn: true };
 };

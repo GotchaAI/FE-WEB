1055407935a656960ff951bcc6b5c2171859efe9
Hwang gend <ghkdtjsdks@naver.com>
2025-05-24 23:41:51 +0900

    feat: 캐릭터 설명 및 svg 상수화

---
+++ src/constants/characterIntroduce.js
@@ -0,0 +1,33 @@
+const { Bongvong } = require("commons/svgs/characters/Bongvong");
+const { Bukbook } = require("commons/svgs/characters/Bukbook");
+const { Myoumoy } = require("commons/svgs/characters/Myoumoy");
+const { Ruru } = require("commons/svgs/characters/Ruru");
+const { Wagwak } = require("commons/svgs/characters/Wagwak");
+
+export const character_info = [
+  {
+    characterName: "묘묘",
+    description: "AI토공지능 로봇이다. 토끼족과 싸우는 것을 즐긴다.",
+    svg: <Myoumoy />,
+  },
+  {
+    characterName: "루루",
+    description: "토끼족의 수장이다. 토끼족을 이끌고 있다.",
+    svg: <Ruru />,
+  },
+  {
+    characterName: "왁왁왁",
+    description: "악어족의 귀요미왁. 악어족의 마지막 생존자다.",
+    svg: <Wagwak />,
+  },
+  {
+    characterName: "봉봉",
+    description: "꽥꽥? 꽥꽥",
+    svg: <Bongvong />,
+  },
+  {
+    characterName: "북북",
+    description: "나....는........북......북이",
+    svg: <Bukbook />,
+  },
+];

029b839cf3b5f220234a1843d482932eb854661a
Hwang gend <ghkdtjsdks@naver.com>
2025-05-25 00:05:18 +0900

    feat: 랭킹 데이터 존재하지 않을때 안내 메시지 출력

--- src/components/home/RankingPreview.jsx
+++ src/components/home/RankingPreview.jsx
@@ -4,15 +4,19 @@ export const RankingPreview = ({ rankingData }) => {
     <div className="ranking-preview-conatiner">
       <h2>랭킹 </h2>
       <ul className="ranking-list">
-        {rankingData.slice(0, 4).map((item, index) => (
-          <li key={index} className="ranking-item">
-            <span className="user-rank-info">
-              {index + 1}. {item.username}
-            </span>
-            <div className="dots" />
-            <span className="score">{item.score} 점</span>
-          </li>
-        ))}
+        {rankingData && rankingData.length > 0 ? (
+          rankingData.slice(0, 4).map((item, index) => (
+            <li key={index} className="ranking-item">
+              <span className="user-rank-info">
+                {index + 1}. {item.username}
+              </span>
+              <div className="dots" />
+              <span className="score">{item.score} 점</span>
+            </li>
+          ))
+        ) : (
+          <p>빈집이네요? 당신이 차지하세요!</p>
+        )}
       </ul>
       <div className="my-score-container">
         <span className="my-score-header">내 점수</span>

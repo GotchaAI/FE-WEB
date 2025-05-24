22f6e36016f18561a166bc51c95f98f6f1957f8d
Hwang gend <ghkdtjsdks@naver.com>
2025-05-24 23:41:20 +0900

    chore: 캐릭터 svg 파일 추가

---
+++ src/commons/svgs/PlusButton.jsx
@@ -0,0 +1,14 @@
+export const PlusButton = ({ onClick }) => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M16 0V32" stroke="black" stroke-width="3" />
+      <path d="M0 16L32 16" stroke="black" stroke-width="3" />
+    </svg>
+  );
+};

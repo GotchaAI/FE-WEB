9cf46490061f821bc7404867ed8eca5fa720a0c9
Hwang gend <ghkdtjsdks@naver.com>
2025-05-25 00:02:17 +0900

    feat: 공지사항이 없을때 문구 출력 구현

--- src/components/home/InformationContainer.jsx
+++ src/components/home/InformationContainer.jsx
@@ -2,12 +2,16 @@ import "styles/components/home/InformationContainer.scss";
 export const InformationContainer = ({ data }) => {
   return (
     <ul className="information-content-layout">
-      {data.map((item, idx) => (
-        <li key={`${item.title}-${idx}`} className="info-item">
-          <span className="title">{item.title}</span>
-          <span className="date">{item.date}</span>
-        </li>
-      ))}
+      {data && data.length > 0 ? (
+        data.map((item, idx) => (
+          <li key={`${item.title}-${idx}`} className="info-item">
+            <span className="title">{item.title}</span>
+            <span className="date">{item.date}</span>
+          </li>
+        ))
+      ) : (
+        <h2>공지사항이 존재하지 않습니다.</h2>
+      )}
     </ul>
   );
 };

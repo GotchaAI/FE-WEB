f7c1127ed4e20756e49836aa732d87a5fba18b28
Hwang gend <ghkdtjsdks@naver.com>
2025-05-25 18:21:22 +0900

    chore: 캐릭터 배경 asset 추가 및 상수 주소 추가

--- src/constants/characterIntroduce.js
+++ src/constants/characterIntroduce.js
@@ -8,26 +8,41 @@ export const character_info = [
   {
     characterName: "묘묘",
     description: "AI토공지능 로봇이다. 토끼족과 싸우는 것을 즐긴다.",
+    detail:
+      "어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고",
     svg: <Myoumoy />,
+    background: "assets/components/introduce/MyoumyoBackground.png",
   },
   {
     characterName: "루루",
     description: "토끼족의 수장이다. 토끼족을 이끌고 있다.",
+    detail:
+      "어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고",
     svg: <Ruru />,
+    background: "assets/components/introduce/RuruBackground.png",
   },
   {
     characterName: "왁왁",
     description: "악어족의 귀요미왁. 악어족의 마지막 생존자다.",
+    detail:
+      "어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고",
     svg: <Wagwak />,
+    background: "assets/components/introduce/WagwakBackground.png",
   },
   {
     characterName: "봉봉",
     description: "꽥꽥? 꽥꽥... 꽥꽥꽥꽥!!!!!",
+    detail:
+      "어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고",
     svg: <Bongvong />,
+    background: "assets/components/introduce/BongvongBackground.png",
   },
   {
     characterName: "북북",
     description: "나....는........북......북이",
+    detail:
+      "어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고",
     svg: <Bukbook />,
+    background: "assets/components/introduce/BukbookBackground.png",
   },
 ];

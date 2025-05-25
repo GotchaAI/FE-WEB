01e17f72d63f907958515fd167f0008532652254
Hwang gend <ghkdtjsdks@naver.com>
2025-05-25 16:25:24 +0900

    feat: 고객센터 답변완료, 대기 UI 구현

--- src/components/home/InformationContainer.jsx
+++ src/components/home/InformationContainer.jsx
@@ -1,10 +1,17 @@
 import "styles/components/home/InformationContainer.scss";
-export const InformationContainer = ({ data }) => {
+
+export const InformationContainer = ({ data, navType }) => {
   return (
     <ul className="information-content-layout">
       {data && data.length > 0 ? (
         data.map((item, idx) => (
           <li key={`${item.title}-${idx}`} className="info-item">
+            {navType === "help" &&
+              (item.answered ? (
+                <span className="answer-done">답변완료</span>
+              ) : (
+                <span className="answer-wait">답변대기</span>
+              ))}
             <span className="title">{item.title}</span>
             <span className="date">{item.date}</span>
           </li>

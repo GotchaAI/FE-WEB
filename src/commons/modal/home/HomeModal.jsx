c7fc6e211adaab9139ef9c2a631b1b9544bae18c
Jiwon Park <parkjiwon0821@naver.com>
2025-05-26 00:37:37 +0900

    refactor: x아이콘 적용

--- src/commons/modal/home/HomeModal.jsx
+++ src/commons/modal/home/HomeModal.jsx
@@ -1,3 +1,4 @@
+import CloseIcon from "commons/svgs/XIcon";
 import { useModalStore } from "store/modal";
 import "styles/commons/modal/HomeModal.scss";
 
@@ -7,16 +8,7 @@ const HomeModal = ({ message, onConfirm }) => {
   return (
     <div className="home-modal">
       <button className="close-btn" onClick={closeModal}>
-        <svg
-          width="16"
-          height="17"
-          viewBox="0 0 16 17"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M0.5 1.25L15 15.75" stroke="black" />
-          <path d="M0.5 15.75L15 1.25" stroke="black" />
-        </svg>
+        <CloseIcon />
       </button>
       <div className="content">{message}</div>
       <button className="confirm-btn" onClick={onConfirm}>

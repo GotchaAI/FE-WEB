25b1059e849b8ef19b092955d5388c0409d27d05
Hwang gend <ghkdtjsdks@naver.com>
2025-05-26 16:11:51 +0900

    feat: 유저정보 받아오는 api 함수 구현

--- src/services/user/user.js
+++ src/services/user/user.js
@@ -1,6 +1,16 @@
 import { NICKNAME_CHECK_API } from "constants/api";
 import { apiInterface } from "services/axiosForm";
 
+export const getUserInfoAPI = async () => {
+  return await apiInterface("get", "/api/v1/users/me/main-info", {}, {}, true);
+};
+
 export const checkNicknameDuplicateAPI = async (nickname) => {
-    return await apiInterface("post", NICKNAME_CHECK_API, { nickname: nickname }, {}, false);
+  return await apiInterface(
+    "post",
+    NICKNAME_CHECK_API,
+    { nickname: nickname },
+    {},
+    false
+  );
 };

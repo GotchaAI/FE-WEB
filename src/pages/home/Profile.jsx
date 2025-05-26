8bfcd2ecde788f2896d1d89390a758ae2230c9a3
Hwang gend <ghkdtjsdks@naver.com>
2025-05-26 16:22:32 +0900

    refactor: 현재 존재하는 사용자 정보 프로필에 적용

--- src/pages/home/Profile.jsx
+++ src/pages/home/Profile.jsx
@@ -1,39 +1,39 @@
 import rabbit from "assets/rabbit-sample.png";
+import useUserInformationStore from "store/userInformation";
 import "styles/components/home/Profile.scss";
 
 const Profile = () => {
-	const userInfo = {
-		exp: 1234,
-		exp_percent: 59.0,
-		nickname: "떡볶이먹고싶",
-		profile_img: rabbit,
-		rank: "1등",
-	};
-
-	return (
-		<div className="profile-container">
-			<div className="exp-container">
-				<progress className="exp-bar" value={userInfo.exp_percent} max="100" />
-				<span className="exp-text">
-					EXP.{userInfo.exp}[{userInfo.exp_percent}%]
-				</span>
-			</div>
-			<div className="profile-user-info-container">
-				<div className="profile-img-container">
-					<img src={userInfo.profile_img} alt="프로필 사진" />
-				</div>
-				<div className="profile-detail-container">
-					<div className="profile-detail">
-						<span className="nickname">{userInfo.nickname}</span>
-						<div className="ranking-container">
-							<div className="left-strip" />
-							<span>{userInfo.rank}</span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  const profile = useUserInformationStore((state) => state.profile);
+  const experience = useUserInformationStore((state) => state.experience);
+  console.log(experience);
+  return (
+    <div className="profile-container">
+      <div className="exp-container">
+        <progress
+          className="exp-bar"
+          value={experience.expProgress * 100}
+          max="100"
+        />
+        <span className="exp-text">
+          EXP.{experience.expInLevel} [{experience.expProgress * 100}%]
+        </span>
+      </div>
+      <div className="profile-user-info-container">
+        <div className="profile-img-container">
+          <img src={rabbit} alt="프로필 사진" />
+        </div>
+        <div className="profile-detail-container">
+          <div className="profile-detail">
+            <span className="nickname">{profile.nickname}</span>
+            <div className="ranking-container">
+              <div className="left-strip" />
+              {/*<span>{userInfo.rank}</span> -> 빠른 시일 내로 자기 랭크와 총 점수 알려주는 api를 파주겠다고 함*/}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;

8582adf8ac7bab4446e71992abb34fc269abd8cd
Hwang gend <ghkdtjsdks@naver.com>
2025-05-28 15:47:21 +0900

    style: 스타일 클래스명 변경

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -80,7 +80,7 @@ const HomePage = () => {
 
   return (
     <div className="home-page-container">
-      <div className="home-fixed-section">
+      <div className="home-top-section">
         <HomeHeader />
 
         <div className="background-container">

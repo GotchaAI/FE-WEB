1bc051d9f23291dd3dec2b8ac5442d3cb1b42ae7
Hwang gend <ghkdtjsdks@naver.com>
2025-05-29 23:15:17 +0900

    refactor: 캐릭터 소개 홈페이지 하위 라우터로 refact

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,10 +1,17 @@
 import HomePage, { loader as homeLoader } from "pages/home/HomePage";
-
+import IntroducePage, {
+  loader as introduceLoader,
+} from "pages/introduce/IntroducePage";
 const home = [
   {
     path: "/",
     element: <HomePage />,
     loader: homeLoader,
+  },
+  {
+    path: "/character-intro",
+    element: <IntroducePage />,
+    loader: introduceLoader,
   },
 ];
 
--- src/routes/root.js
+++ src/routes/root.js
@@ -3,14 +3,13 @@ import { createBrowserRouter } from "react-router-dom";
 import auth from "routes/auth";
 import home from "routes/home";
 import lobby from "routes/lobby";
-import introduce from "routes/introduce";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     loader: rootLoader,
-    children: [...auth, ...home, ...lobby, ...introduce],
+    children: [...auth, ...home, ...lobby],
   },
 ]);
 

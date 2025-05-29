cfe6a4b0aa52a0b3bfc8425b09e9a355d658ab7b
whitecity01 <kh081104@gmail.com>
2025-05-30 04:06:10 +0900

    feat: 게임 소켓 토큰 및 에러 구독 로직 추가

--- src/hooks/useGameSocket.js
+++ src/hooks/useGameSocket.js
@@ -1,53 +1,68 @@
-import { Client } from '@stomp/stompjs';
-import { useEffect, useRef } from 'react';
-import SockJS from 'sockjs-client';
+import { Client } from "@stomp/stompjs";
+import { useEffect } from "react";
+import SockJS from "sockjs-client";
+import { useGameSocketStore } from "store/socket";
+import { getAuthToken } from "utils/token";
 
 /**
  * useGameSocket 커스텀 훅
  *
  * 게임 웹소켓 서버에 연결
- * nickName과 roomId를 기반으로 초기 연결을 수행
  * 서버에 접속 정보를 publish
  * 연결 실패 시 5000ms 간격으로 재연결 시도
  */
 
 const SOCKET_BASE_URL = process.env.REACT_APP_SOCKET_BASE_URL;
 
-const useGameSocket = ({ nickName, roomId }) => {
-  const stompClientRef = useRef(null);
+const useGameSocket = () => {
+  const { setStompClient, clearStompClient } = useGameSocketStore();
+  const { accessToken } = getAuthToken();
+  const userUuid = 5; // TODO : uuid 저장 로직 추가 시 적용
 
   useEffect(() => {
     const client = new Client({
-      webSocketFactory: () => new SockJS(`${SOCKET_BASE_URL}${process.env.REACT_APP_WS_ENDPOINT}`),
+      webSocketFactory: () =>
+        new SockJS(`${SOCKET_BASE_URL}${process.env.REACT_APP_WS_ENDPOINT}`),
+      connectHeaders: {
+        Authorization: accessToken,
+      },
       reconnectDelay: 5000,
-      debug: (str) => console.log('STOMP 핸드쉐이킹', str),
+      debug: (str) => console.log("STOMP 핸드쉐이킹", str),
     });
 
     client.onConnect = () => {
-      console.log('Connected!');
+      console.log("Connected!");
 
-      // 초기 연결 알림 전송
-      client.publish({
-        destination: '/pub/connect',
-        body: JSON.stringify({ nickName: nickName }),
+      // TODO: 백엔드 테스트 후 제거 예정
+      client.subscribe("/user/queue/errors", (message) => {
+        console.log(
+          "사용자별 에러 로그 :" + message + "상세 내용" + message.body
+        );
       });
 
-    }
+      // 주요 에러 핸들러
+      client.subscribe(`/user/${userUuid}/queue/errors`, (message) => {
+        console.log(
+          "사용자별 에러 로그 :" + message + "상세 내용" + message.body
+        );
+      });
+    };
 
     // stomp 에러 디버깅
     client.onStompError = (frame) => {
-      console.log('STOMP 오류', frame);
-    }
+      console.log("STOMP 오류", frame);
+    };
 
     // 웹소켓 연결
     client.activate();
-    stompClientRef.current = client;
+    setStompClient(client);
 
     return () => {
       // 웹소켓 해제
       client.deactivate();
-    }
-  }, [nickName, roomId]);
+      clearStompClient();
+    };
+  }, []);
 
   return {};
 };

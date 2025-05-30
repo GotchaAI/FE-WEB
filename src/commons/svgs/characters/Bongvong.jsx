22c479bf390c9ce5f992703ec15b0d79772357d2
whitecity01 <kh081104@gmail.com>
2025-05-31 01:52:10 +0900

    fix: 속성명 변경

--- src/commons/svgs/characters/Bongvong.jsx
+++ src/commons/svgs/characters/Bongvong.jsx
@@ -43,9 +43,9 @@ export const Bongvong = () => {
           width="316.066"
           height="304.655"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"

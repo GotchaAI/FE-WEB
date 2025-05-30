f89ea36f91868c3fde8fb399f7c2489cafc26c35
whitecity01 <kh081104@gmail.com>
2025-05-31 06:29:16 +0900

    test: 컨텍스트 컴포넌트 테스트용 코드 추가

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,7 @@
+import ContextMenu from "commons/contextMenu/ContextMenu";
 import ScrollToTop from "components/common/ScrollToTop";
 import { HomeHeader } from "components/home/HomeHeader";
+import useContextMenu from "hooks/contextmenu/useContextMenu";
 import { useEffect } from "react";
 import { Outlet, useLoaderData } from "react-router-dom";
 import { tokenReissueAPI } from "services/auth/auth";
@@ -12,11 +14,42 @@ const HomePage = () => {
     window.scrollTo(0, 0);
   }, []);
 
+  const a = () => console.log("a");
+  const b = () => console.log("b");
+  const c = () => console.log("c");
+
+  const { isOpen, menus, position, openMenu, closeMenu } = useContextMenu();
+
+  const menuList = [
+    {
+      label: "a버튼",
+      action: a,
+    },
+    {
+      label: "b버튼",
+      action: b,
+    },
+    {
+      label: "c버튼",
+      action: c,
+    },
+  ];
+
+  const menuHandler = (e) => {
+    openMenu(e.pageX, e.pageY, menuList);
+  };
+
   return (
     <div className="home-page-container">
       <ScrollToTop />
+
       <HomeHeader isSignIn={isSignIn} />
+
       <Outlet />
+      {isOpen && (
+        <ContextMenu menus={menus} position={position} onClose={closeMenu} />
+      )}
+      <button onClick={menuHandler}>테스트버튼</button>
     </div>
   );
 };

d0d0e33c86582944f1d88993e5a0ad20cbdb903a
Jiwon Park <parkjiwon0821@naver.com>
2025-05-30 21:35:13 +0900

    design: 버튼 호버효과 추가

--- src/commons/svgs/OkayButton.jsx
+++ src/commons/svgs/OkayButton.jsx
@@ -1,13 +1,22 @@
 import okay_btn from "assets/commons/okay-btn.png";
+import okay_hover_btn from "assets/commons/okay-btn-hover.png";
+import { useState } from "react";
 import "styles/commons/svgs/OkayButton.scss";
 
 const OkayButton = ({ onClick }) => {
+  const [isHover, setIsHover] = useState(false);
+
   const handleClick = (e) => {
     onClick?.(e); // 기존 클릭 핸들러 호출
   };
 
   return (
-    <button className="okay-svg-btn" onClick={handleClick}>
+    <button
+      className="okay-svg-btn"
+      onClick={handleClick}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
       <svg
         width="345"
         height="64"
@@ -18,7 +27,7 @@ const OkayButton = ({ onClick }) => {
         <path
           className="svg-main-fill"
           d="M344.5 0.5V31.5625C344.5 49.2011 330.201 63.4999 312.562 63.5H0.5V0.5H344.5Z"
-          fill="#FFC466"
+          fill={isHover ? "#f0a000" : "#FFC466"}
           stroke="black"
         />
         <path
@@ -30,7 +39,7 @@ const OkayButton = ({ onClick }) => {
       </svg>
 
       <span className="label">
-        <img src={okay_btn} alt="확인 버튼" className="start-btn-img" />
+        <img src={isHover ? okay_hover_btn : okay_btn} alt="확인 버튼" />
       </span>
     </button>
   );

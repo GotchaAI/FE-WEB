da60a26303e82637af4a9b25d36cd6cdf0e67992
whitecity01 <kh081104@gmail.com>
2025-06-01 00:43:22 +0900

    feat: menus 불필요한 상태 변경 방지 조건문 추가

--- src/hooks/contextmenu/useContextMenu.js
+++ src/hooks/contextmenu/useContextMenu.js
@@ -27,7 +27,9 @@ const useContextMenu = () => {
 
   const openMenu = (x, y, newMenus) => {
     setPosition({ x, y });
-    setMenus(newMenus);
+    if (menus !== newMenus) {
+      setMenus(newMenus);
+    }
     setIsOpen(true);
   };
 

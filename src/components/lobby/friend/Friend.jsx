b062c153f9f33c2cc641489e8ea0b2a63191f4f0
Hwang gend <ghkdtjsdks@naver.com>
2025-06-05 01:28:47 +0900

    feat: 친구 컴포넌트 전환 구현

--- src/components/lobby/friend/Friend.jsx
+++ src/components/lobby/friend/Friend.jsx
@@ -1,24 +1,41 @@
 import SearchIcon from "commons/svgs/SearchIcon";
 import FriendList from "components/lobby/friend/FriendList";
+import { useState } from "react";
 import "styles/components/lobby/friend/Friend.scss";
 
 const Friend = () => {
+  const [friendActionType, setFriendActionType] = useState("list");
+
   return (
     <div className="friend-container">
       <h1>friend</h1>
 
       <div className="friend-option-container">
-        <button className="friend-list-btn">친구 목록</button>
-        <button className="friend-request-btn">친구 신청</button>
+        <button
+          className="friend-list-btn"
+          onClick={() => setFriendActionType("list")}
+        >
+          친구 목록
+        </button>
+        <button
+          className="friend-request-btn"
+          onClick={() => setFriendActionType("apply")}
+        >
+          친구 신청
+        </button>
       </div>
 
       <div className="friend-search-container">
         <input className="friend-search-form" placeholder="검색" />
         <SearchIcon />
       </div>
-      <div className="friend-list-container">
-        <FriendList />
-      </div>
+      {friendActionType === "list" ? (
+        <div className="friend-list-container">
+          <FriendList />
+        </div>
+      ) : (
+        <div className="friend-list-container">ㅎㅇ</div>
+      )}
     </div>
   );
 };

a47771acaf2702e7117ce17cc39a77e1599a17a3
Jiwon Park <parkjiwon0821@naver.com>
2025-06-09 22:37:12 +0900

    feat: 게임2로비페이지 구현

--- src/pages/game/Game2LobbyPage.jsx
+++ src/pages/game/Game2LobbyPage.jsx
@@ -1,5 +1,21 @@
+import game2_rabbit from "assets/components/lobby/game2-lobby-rabbit.png";
+import GameStartButton from "commons/svgs/GameStartButton";
+import "styles/pages/game/Game2LobbyPage.scss";
+
 const Game2LobbyPage = () => {
-  return <div>ㅎㅇ 게임1이야</div>;
+  // 게임 시작 요청
+  const gameStartHandler = () => {
+    console.log("game2 시작~");
+  };
+
+  return (
+    <div className="game2-lobby-container">
+      <img src={game2_rabbit} alt="Game 2 Rabbit" className="rabbit-image" />
+      <div className="game2-start-btn">
+        <GameStartButton onClick={gameStartHandler} />
+      </div>
+    </div>
+  );
 };
 
 export default Game2LobbyPage;

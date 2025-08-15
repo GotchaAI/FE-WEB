daa8a4bf24d33bdd852088085ad416b3c620178b
whitecity01 <kh081104@gmail.com>
2025-08-15 13:40:50 +0900

    fix: MacOS 환경에서 한글이 두 번 입력되는 현상 해결

--- src/components/lobby/LobbyChatting.jsx
+++ src/components/lobby/LobbyChatting.jsx
@@ -138,15 +138,12 @@ const LobbyChatting = ({
   }, 100);
 
   const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      if (e.shiftKey) {
-        return;
-      } else {
-        e.preventDefault();
-        handleSendMessage();
-      }
+    if (e.key === "Enter" && !e.nativeEvent.isComposing) {
+      e.preventDefault();
+      handleSendMessage();
     }
   };
+
   return (
     <div className="lobby-chatting-container">
       <img

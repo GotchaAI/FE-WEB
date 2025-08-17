c683ad278fa9fe322d5da7a3e6d1b84dc4d9a464
Hwang gend <ghkdtjsdks@naver.com>
2025-08-17 16:55:38 +0900

    feat: 닉네임 변경 컴포넌트 구현

--- src/components/lobby/mypage/EditNickname.jsx
+++ src/components/lobby/mypage/EditNickname.jsx
@@ -1,3 +1,26 @@
+import CloseIcon from "commons/svgs/XIcon";
+import "styles/components/lobby/mypage/EditNickname.scss";
 export const EditNickname = () => {
-  return <>닉네임수정 컴포넌트 입니다~</>;
+  return (
+    <div className="edit-nickname-container">
+      <button className="close-btn">
+        <CloseIcon />
+      </button>
+      <span className="edit-nickname-title">닉네임 설정</span>
+      <p className="edit-nickname-notification">
+        (최대 한글 6자 영문 12자 입력 가능)
+        <br />
+        변경 후 24시간 뒤 재변경이 가능합니다.
+      </p>
+      <div className="nickname-input-wrapper">
+        <input
+          type="text"
+          placeholder="닉네임적는곳"
+          className="nickname-input"
+        />
+        <button className="duplicate-check-btn">중복확인</button>
+      </div>
+      <button className="submit-nickname-btn">변경하기</button>
+    </div>
+  );
 };

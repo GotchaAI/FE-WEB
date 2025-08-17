c12a907e328c95be73c20e2ae931b64edd5e8930
whitecity01 <kh081104@gmail.com>
2025-08-17 17:39:27 +0900

    feat: 음향 설정 기능 추가

--- src/components/lobby/setting/SoundSetting.jsx
+++ src/components/lobby/setting/SoundSetting.jsx
@@ -1,14 +1,17 @@
 import VolumeControl from "commons/ui/inputs/VolumeControl";
 import { useEffect, useState } from "react";
+import { audioStore } from "store/audio";
 import "styles/components/lobby/setting/SoundSetting.scss";
 
 const SoundSetting = ({ save, setSave }) => {
   const [background, setBackground] = useState(50);
   const [soundEffect, setSoundEffect] = useState(50);
 
+  const { setVolume } = audioStore.getState();
+
   useEffect(() => {
     if (!save) return;
-
+    setVolume(background / 100); // 0~1 의 범위
     // 저장
     setSave((prev) => !prev);
   });

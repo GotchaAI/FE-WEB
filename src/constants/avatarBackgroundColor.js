e7b55c7a4bf729d52fc91a5d9570d02c5dc49682
Hwang gend <ghkdtjsdks@naver.com>
2025-09-01 22:38:12 +0900

    chore: 아바타 수정 초기값 변경 및 스크롤바 두께 수정

--- src/constants/avatarBackgroundColor.js
+++ src/constants/avatarBackgroundColor.js
@@ -1,5 +1,5 @@
 export const avatarBackgroundColorList = [
-  "#FFC466",
+  "#FFDA37",
   "#F6AA30",
   "#F28110",
   "#7E5615",
@@ -7,7 +7,7 @@ export const avatarBackgroundColorList = [
   "#E9FF9F",
   "#89C881",
   "#AAE8FF",
-  "#FFDA37",
+  "#FFC466",
   "#F4EC6F",
   "#000000",
   "#999999",

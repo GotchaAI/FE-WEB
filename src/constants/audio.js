788d931590e90f09670b54b7016772c021ea6e6a
Jiwon Park <parkjiwon0821@naver.com>
2025-09-03 19:27:47 +0900

    feat: game2 오디오파일 상수 추가

--- src/constants/audio.js
+++ src/constants/audio.js
@@ -3,6 +3,15 @@ import battle_bgm from "assets/audio/battle.mp3";
 import drawing_bgm from "assets/audio/drawing.mp3";
 import description_bgm from "assets/audio/description.mp3";
 import hook_sfx from "assets/audio/sfx/hook.mp3";
+
+import drawing_bgm2 from "assets/audio/drawing2.mp3";
+import fail from "assets/audio/fail.mp3";
+import pass from "assets/audio/pass.mp3";
+import pray from "assets/audio/pray.mp3";
+import opening from "assets/audio/game2opening.wav";
+import huh from "assets/audio/huh.wav";
+import okay_rabbit from "assets/audio/okay_rabbit.mp3";
+
 /**
  * 오디오 파일 상수
  *
@@ -44,10 +53,59 @@ const hookSFX = {
   src: hook_sfx,
 };
 
+const game2DrawingBGM = {
+  type: "bgm",
+  isLoop: true,
+  src: drawing_bgm2,
+}
+
+const failSFX = {
+  type: "sfx",
+  isLoop: false,
+  src: fail,
+};
+
+const passSFX = {
+  type: "sfx",
+  isLoop: false,
+  src: pass,
+};
+
+const praySFX = {
+  type: "sfx",
+  isLoop: false,
+  src: pray,
+};
+
+const openingSFX = {
+  type: "sfx",
+  isLoop: false,
+  src: opening,
+};
+
+const huhSFX = {
+  type: "sfx",
+  isLoop: false,
+  src: huh,
+};
+
+const okayRabbitSFX = {
+  type: "sfx",
+  isLoop: false,
+  src: okay_rabbit,
+};
+
 export {
   LobbyBGM,
   game1DrawingBGM,
   game1BattleBGM,
   game2DescriptionBGM,
   hookSFX,
+  game2DrawingBGM,
+  failSFX,
+  passSFX,
+  praySFX,
+  openingSFX,
+  huhSFX,
+  okayRabbitSFX,
 };

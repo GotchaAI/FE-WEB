e46fbe1abcd72c95bbdf112cd580c861162c70bf
Jiwon Park <parkjiwon0821@naver.com>
2025-09-10 19:54:37 +0900

    feat: 아이콘및 목데이터 추가

--- src/pages/home/AnnounceDetailPage.jsx
+++ src/pages/home/AnnounceDetailPage.jsx
@@ -1,6 +1,21 @@
+import AnnounceBear from "commons/svgs/AnnounceBear";
 import CloseButton from "commons/ui/button/CloseButton";
 import { useNavigate } from "react-router-dom";
 import "styles/pages/home/AnnounceDetailPage.scss";
+
+const mockAnnounce = {
+  id: 1,
+  title: "서버 점검 및 업데이트",
+  date: "2025.07.21",
+  content: `
+    어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌
+    asdfasfasfd a sdfa asdfa asdf assdfasdvasdvas
+    어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌
+    asdfasfasfd a sdfa asdfa asdf assdfasdvasdvas
+    어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌
+    asdfasfasfd a sdfa asdfa asdf assdfasdvasdvas
+  `,
+};
 
 const AnnounceDetailPage = () => {
   const naviate = useNavigate();
@@ -14,22 +29,15 @@ const AnnounceDetailPage = () => {
       <h1 className="announce-detail-title">공지사항</h1>
       <article className="announce-detail-container">
         <header className="announce-detail-header">
-          <span className="meta-date">2025.07.21</span>
+          <span className="meta-date">{mockAnnounce.date}</span>
           <CloseButton onClick={closeDetailpage} />
         </header>
         <div className="announce-detail-content">
-          <div className="content-title">서버 점검 및 업데이트</div>
-          <div className="content-description">
-            어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌 asdfasfasfd a sdfa asdfa
-            asdf assdfasdvasdvas 어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌
-            asdfasfasfd a sdfa asdfa asdf assdfasdvasdvas
-            어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌 asdfasfasfd a sdfa asdfa
-            asdf assdfasdvasdvas 어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌
-            asdfasfasfd a sdfa asdfa asdf assdfasdvasdvas
-            어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌 asdfasfasfd a sdfa asdfa
-            asdf assdfasdvasdvas 어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌
-            asdfasfasfd a sdfa asdfa asdf assdfasdvasdvas
+          <div className="content-title">
+            <AnnounceBear />
+            {mockAnnounce.title}
           </div>
+          <div className="content-description">{mockAnnounce.content}</div>
         </div>
       </article>
     </div>

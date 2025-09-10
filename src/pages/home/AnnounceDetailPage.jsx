a1ca15e02bf7b5e31ed455b106bb473d9dadda33
Jiwon Park <parkjiwon0821@naver.com>
2025-09-10 19:39:14 +0900

    feat: 공지사항 세부페이지 css 추가

--- src/pages/home/AnnounceDetailPage.jsx
+++ src/pages/home/AnnounceDetailPage.jsx
@@ -17,7 +17,20 @@ const AnnounceDetailPage = () => {
           <span className="meta-date">2025.07.21</span>
           <CloseButton onClick={closeDetailpage} />
         </header>
-        <div className="announce-detail-content"></div>
+        <div className="announce-detail-content">
+          <div className="content-title">서버 점검 및 업데이트</div>
+          <div className="content-description">
+            어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌 asdfasfasfd a sdfa asdfa
+            asdf assdfasdvasdvas 어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌
+            asdfasfasfd a sdfa asdfa asdf assdfasdvasdvas
+            어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌 asdfasfasfd a sdfa asdfa
+            asdf assdfasdvasdvas 어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌
+            asdfasfasfd a sdfa asdfa asdf assdfasdvasdvas
+            어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌 asdfasfasfd a sdfa asdfa
+            asdf assdfasdvasdvas 어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌
+            asdfasfasfd a sdfa asdfa asdf assdfasdvasdvas
+          </div>
+        </div>
       </article>
     </div>
   );

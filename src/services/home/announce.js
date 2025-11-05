eeef0afeb4e93b5a67bfde74d5064371c1db7b7d
Jiwon Park <parkjiwon0821@naver.com>
2025-11-05 16:08:46 +0900

    feat: 공지사항 조회 api 연결

--- src/services/home/announce.js
+++ src/services/home/announce.js
@@ -3,3 +3,7 @@ import { apiInterface } from "services/axiosForm";
 export const getAnnounceListAPI = async (params = {}) => {
   return await apiInterface("get", "/api/v1/notifications", {}, params, false);
 };
+
+export const getAnnounceDetailAPI = async (notificationId) => {
+  return await apiInterface("get", `/api/v1/notifications/${notificationId}`, {}, {}, false);
+};

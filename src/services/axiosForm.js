80d7d07d5b122150e8c3c8bd8b68798ce160f1f0
whitecity01 <kh081104@gmail.com>
2025-11-05 18:08:53 +0900

    feat: csrfToken 인터셉터 추가

--- src/services/axiosForm.js
+++ src/services/axiosForm.js
@@ -1,15 +1,18 @@
-import axios from "axios";
-import { LOCAL_SERVER_IP } from "constants/api";
-import { REFRESH_TOKEN_EXPIRED } from "constants/errorCode";
-import { SIGN_IN_URL } from "constants/url";
-import { tokenReissueAPI } from "services/auth/auth";
-import { getAuthToken, isTokenExpired } from "utils/token";
+import axios from 'axios';
+import { LOCAL_SERVER_IP } from 'constants/api';
+import {
+  CSRF_TOKEN_NOT_FOUND,
+  REFRESH_TOKEN_EXPIRED,
+} from 'constants/errorCode';
+import { SIGN_IN_URL } from 'constants/url';
+import { csrfTokenRequestAPI, tokenReissueAPI } from 'services/auth/auth';
+import { getAuthToken, isTokenExpired, setCsrfToken } from 'utils/token';
 
 const baseConfig = {
   baseURL: LOCAL_SERVER_IP,
   timeout: 60000,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
   withCredentials: true,
 };
@@ -26,6 +29,35 @@ const multipartConfig = {
 const tokenInstance = axios.create(baseConfig); // 토큰 인터셉터 적용
 const instance = axios.create(baseConfig); // 인터셉터 미적용
 const multipartInstance = axios.create(multipartConfig);
+
+function attachCsrfTokenInterceptors(instance) {
+  // --- Response 인터셉터: csrf토큰 에러 시 재발급 후 요청 ---
+  instance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const originalRequest = error.config;
+      if (
+        error.response?.status === 403 &&
+        error.response?.data.code === CSRF_TOKEN_NOT_FOUND
+      ) {
+        try {
+          const res = await csrfTokenRequestAPI();
+          const csrfToken = res.csrfToken;
+          setCsrfToken(csrfToken);
+
+          originalRequest.headers = {
+            ...originalRequest.headers,
+            'X-XSRF-TOKEN': `${csrfToken}`,
+          };
+          return axios.request(originalRequest);
+        } catch (err) {
+          return Promise.reject(err);
+        }
+      }
+    },
+  );
+}
+
 function attachTokenInterceptors(instance) {
   // --- Request 인터셉터: Access Token이 만료되었으면 재발급 ---
   instance.interceptors.request.use(
@@ -41,22 +73,22 @@ function attachTokenInterceptors(instance) {
           const newExpireTime = res.expiredAt;
           setAccessToken(newToken, newExpireTime);
 
-          config.headers["Authorization"] = `${newToken}`;
+          config.headers['Authorization'] = `${newToken}`;
         } catch (error) {
-          console.error("토큰 재발급 실패", error);
+          console.error('토큰 재발급 실패', error);
           window.location.href = SIGN_IN_URL;
           return Promise.reject(error);
         }
       } else {
-        config.headers["Authorization"] = `${accessToken}`;
+        config.headers['Authorization'] = `${accessToken}`;
       }
 
       return config;
     },
     (error) => {
-      console.error("Request 인터셉터 에러:", error);
+      console.error('Request 인터셉터 에러:', error);
       return Promise.reject(error);
-    }
+    },
   );
 
   // --- Response 인터셉터: Access Token 만료(401) 시 재시도 ---
@@ -91,10 +123,10 @@ function attachTokenInterceptors(instance) {
       }
 
       return Promise.reject(error);
-    }
+    },
   );
 }
-
+attachCsrfTokenInterceptors(instance);
 attachTokenInterceptors(tokenInstance);
 attachTokenInterceptors(multipartInstance);
 // API 요청 함수 (옵션으로 인터셉터 선택)
@@ -103,14 +135,14 @@ export const apiInterface = async (
   url,
   data = {},
   params = {},
-  useToken = true
+  useToken = true,
 ) => {
   const instanceType = useToken ? tokenInstance : instance;
   try {
     const res = await instanceType({ method, url, data, params });
     return res.data;
   } catch (e) {
-    console.error("API 요청 중 오류:", e);
+    console.error('API 요청 중 오류:', e);
     throw e;
   }
 };
@@ -120,13 +152,13 @@ export const multipartApiInterface = async (
   method,
   url,
   data = {},
-  params = {}
+  params = {},
 ) => {
   try {
     const res = await multipartInstance({ method, url, data, params });
     return res.data;
   } catch (e) {
-    console.error("API 요청 중 오류:", e);
+    console.error('API 요청 중 오류:', e);
     throw e;
   }
 };

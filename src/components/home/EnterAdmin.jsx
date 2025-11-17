e1a1ebbf5ecb08c9c7e67411879668e365fa7dde
Hwang gend <ghkdtjsdks@naver.com>
2025-11-18 01:59:32 +0900

    refactor: 관리자 입장 버튼 수정

--- src/components/home/EnterAdmin.jsx
+++ src/components/home/EnterAdmin.jsx
@@ -1,4 +1,5 @@
 import { AdminButton } from "commons/svgs/AdminButton";
+import { ADMIN_URL } from "constants/url";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "styles/components/home/EnterAdmin.scss";
@@ -7,7 +8,7 @@ export default function EnterAdmin() {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate("/admin");
+    navigate(ADMIN_URL);
   };
 
   const handleKeyDown = (e) => {
@@ -23,8 +24,6 @@ export default function EnterAdmin() {
       type="button"
       onClick={handleClick}
       onKeyDown={handleKeyDown}
-      aria-label="관리자 페이지로 이동"
-      title="관리자 페이지로 이동"
     >
       <AdminButton />
     </button>

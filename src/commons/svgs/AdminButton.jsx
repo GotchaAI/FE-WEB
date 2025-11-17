f154ab192db3c29bab1e84deee89ba871c5ba456
Hwang gend <ghkdtjsdks@naver.com>
2025-11-17 23:50:46 +0900

    chore: 관리자 진입 버튼 svg 추가

---
+++ src/commons/svgs/AdminButton.jsx
@@ -0,0 +1,7 @@
+export const AdminButton = () => {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor">
+      <path d="M12 2 2 7v6c0 5 5 9 10 9s10-4 10-9V7l-10-5zM12 18a2.5 2.5 0 110-5 2.5 2.5 0 010 5zm4-8H8V9a4 4 0 018 0v1z" />
+    </svg>
+  );
+};

1bfebc5b48b8158b30576e5243b187d44ac316fc
Hwang gend <ghkdtjsdks@naver.com>
2025-11-18 22:17:21 +0900

    refactor: 스크롤 초기화 예외상황 커버

--- src/components/common/ScrollToTop.jsx
+++ src/components/common/ScrollToTop.jsx
@@ -3,8 +3,12 @@ import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
-
   useEffect(() => {
+    const rootContainer = document.querySelector(".root-layout-container");
+    if (rootContainer) {
+      rootContainer.scrollTo(0, 0);
+    }
+
     window.scrollTo(0, 0);
   }, [pathname]);
 

c47d12275f3586bd6980fd87f2cbdab6255d8fce
Hwang gend <ghkdtjsdks@naver.com>
2025-11-18 13:47:40 +0900

    feat: 공지사항 수정, 게시 api 연결

--- src/services/admin/admin.js
+++ src/services/admin/admin.js
@@ -1,11 +1,31 @@
-import { ADMIN_ANNOUNCE_DELETE_API } from "constants/api";
+import { ADMIN_ANNOUNCE_MANAGE_API } from "constants/api";
 import { apiInterface } from "services/axiosForm";
 
 export const eraseNotification = async (notificationId) => {
   return await apiInterface(
     "delete",
-    `${ADMIN_ANNOUNCE_DELETE_API}/${notificationId}`,
+    `${ADMIN_ANNOUNCE_MANAGE_API}/${notificationId}`,
     { notificationId },
+    {},
+    true
+  );
+};
+
+export const updateAnnounceAPI = async (notification) => {
+  return await apiInterface(
+    "put",
+    `${ADMIN_ANNOUNCE_MANAGE_API}/${notification.id}`,
+    { title: notification.title, content: notification.content },
+    {},
+    true
+  );
+};
+
+export const postAnnounceAPI = async (notification) => {
+  return await apiInterface(
+    "post",
+    `${ADMIN_ANNOUNCE_MANAGE_API}`,
+    { title: notification.title, content: notification.content },
     {},
     true
   );

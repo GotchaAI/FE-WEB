5589d9fe4b9b5c80383a622816bfb4941909ea99
Hwang gend <ghkdtjsdks@naver.com>
2025-11-18 14:50:00 +0900

    feat: 관리자 홈 추가 및 야가다 토끼 사진 추가

---
+++ src/pages/admin/AdminHome.jsx
@@ -0,0 +1,9 @@
+import "styles/pages/admin/AdminHome.scss";
+import yagadaRabbit from "assets/components/admin/yagada-rabbit.png";
+export const AdminHome = () => {
+  return (
+    <div className="admin-home-container">
+      <img src={yagadaRabbit} />
+    </div>
+  );
+};
